14b73e0dc15b0bc6f52e538b9476a4e5b6a4721d
Yosdany <yosdanybm@gmail.com>
2023-08-13 18:49:12 -0400

    Config theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+import dayjs from 'dayjs';
+import {
+  ConfigProvider
+} from 'antd';
+import esES from 'antd/locale/es_ES';
+import 'dayjs/locale/es-us';
+import utc from 'dayjs/plugin/utc'
+import { themeCustom } from './theme/theme';
+import Layout from './layout/Layout';
+import Home from './modules/home/container/Home';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+dayjs.locale('es');
+dayjs.extend(utc);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => (
+  <ConfigProvider theme={themeCustom} locale={esES}>
+    <Layout>
+      <Home />
+    </Layout>
+  </ConfigProvider>
+)
 
 export default App
